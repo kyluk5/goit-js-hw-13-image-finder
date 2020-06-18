24fc0e4f0a47b9a4b7c40500f9ad5ed2e4afa8a0
Andrii Kulyk <kyluk5@gmail.com>
2020-06-19 02:01:53 +0300

    fulf_done

--- src/js/workDom.js
+++ src/js/workDom.js
@@ -1,22 +1,36 @@
 import search_form from '../templates/search-form.hbs';
 import gallery from '../templates/gallery.hbs';
+import image_card from '../templates/image_card.hbs';
 import debounce from 'lodash.debounce';
 import pixabayApi from './apiService';
-import image_card from '../templates/image_card.hbs';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
 // =========================================================
 document.body.insertAdjacentHTML('beforeend', search_form());
 document.body.insertAdjacentHTML('beforeend', gallery());
 // =========================================================
 const galleruUl = document.querySelector('.gallery');
-// =========================================================
 const input = document.querySelector('.image-input');
+// =========================================================
 input.addEventListener('input', debounce(inguiry, 500));
 function inguiry(e) {
-  galleruUl.innerHTML = '';
+  e.preventDefault();
+  if (e.target.value === '') {
+    debounce((galleruUl.innerHTML = ''), 500);
+  }
   pixabayApi(e.target.value)
     .then(data => {
       const object = data.hits.map(item => image_card(item)).join('');
       galleruUl.innerHTML = object;
     })
-    .catch(err => console.warn(err));
+    .catch(
+      error({
+        closer: false,
+        sticker: false,
+        delay: 200,
+        remove: true,
+        destroy: true,
+        text: 'Please enter a more specifically query!',
+      }),
+    );
 }
