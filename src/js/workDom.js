cdd4ce8a374a0f8fcb4d24da381f86909ea4143e
Andrii Kulyk <kyluk5@gmail.com>
2020-06-19 19:04:49 +0300

    fixed_resourch_page

--- src/js/workDom.js
+++ src/js/workDom.js
@@ -1,36 +1,33 @@
 import search_form from '../templates/search-form.hbs';
 import gallery from '../templates/gallery.hbs';
 import image_card from '../templates/image_card.hbs';
+import loadMore from '../templates/loadMore.hbs';
 import debounce from 'lodash.debounce';
 import pixabayApi from './apiService';
-import '@pnotify/core/dist/BrightTheme.css';
-import { error } from '@pnotify/core';
 // =========================================================
 document.body.insertAdjacentHTML('beforeend', search_form());
 document.body.insertAdjacentHTML('beforeend', gallery());
+document.body.insertAdjacentHTML('beforeend', loadMore());
 // =========================================================
 const galleruUl = document.querySelector('.gallery');
 const input = document.querySelector('.image-input');
 // =========================================================
 input.addEventListener('input', debounce(inguiry, 500));
+// =========================================================
 function inguiry(e) {
   e.preventDefault();
+  galleruUl.innerHTML = '';
   if (e.target.value === '') {
-    debounce((galleruUl.innerHTML = ''), 500);
+    document.querySelector('.load_more').classList.add('hidden');
+    return;
   }
-  pixabayApi(e.target.value)
+  pixabayApi([e.target.value], [])
     .then(data => {
       const object = data.hits.map(item => image_card(item)).join('');
-      galleruUl.innerHTML = object;
+      galleruUl.insertAdjacentHTML('beforeend', object);
+      if (data.total > 0) {
+        document.querySelector('.load_more').classList.remove('hidden');
+      }
     })
-    .catch(
-      error({
-        closer: false,
-        sticker: false,
-        delay: 200,
-        remove: true,
-        destroy: true,
-        text: 'Please enter a more specifically query!',
-      }),
-    );
+    .catch(err => console.warn(err));
 }
